28ace87b65367abe578205bdda3073a459cde289
iubr <iubr@umk.pl>
2025-12-15 16:08:24 +0100

    setup.py to explicitly define include_dirs

--- gsed/unitconversion.pyx
+++ gsed/unitconversion.pyx
@@ -1,6 +1,6 @@
 # distutils: language = c++
 
-cdef extern from "gsed/unitconversion_cpp.h":
+cdef extern from "unitconversion_cpp.h":
     double magToJy_cpp(double magnitude)
 
 def cmagToJy(double magnitude):
