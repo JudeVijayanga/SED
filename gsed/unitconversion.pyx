dd1a55bd2b2987b4499c7e13369b36b590ef44d7
iubr <iubr@umk.pl>
2025-12-15 16:05:52 +0100

    package-relative path

--- gsed/unitconversion.pyx
+++ gsed/unitconversion.pyx
@@ -1,7 +1,6 @@
 # distutils: language = c++
-# distutils: include_dirs = gsed
 
-cdef extern from "unitconversion_cpp.h":
+cdef extern from "gsed/unitconversion_cpp.h":
     double magToJy_cpp(double magnitude)
 
 def cmagToJy(double magnitude):
